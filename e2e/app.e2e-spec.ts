1eb3f6f2d30cfe072530c235b86982f0b220ee7e
icnbrave <fuzhiyong19861210@163.com>
2017-06-01 20:49:29 +0800

    1. init layout.

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,10 +1,10 @@
-import { PlantingCmsPage } from './app.po';
+import { CoreUIPage } from './app.po';
 
-describe('planting-cms App', () => {
-  let page: PlantingCmsPage;
+describe('core-ui App', function() {
+  let page: CoreUIPage;
 
   beforeEach(() => {
-    page = new PlantingCmsPage();
+    page = new CoreUIPage();
   });
 
   it('should display message saying app works', () => {
--- e2e/app.po.ts
+++ e2e/app.po.ts
@@ -1,6 +1,6 @@
-import { browser, by, element } from 'protractor';
+import { browser, element, by } from 'protractor/globals';
 
-export class PlantingCmsPage {
+export class CoreUIPage {
   navigateTo() {
     return browser.get('/');
   }
